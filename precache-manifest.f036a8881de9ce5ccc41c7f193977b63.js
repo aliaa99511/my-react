c6a06f854b0b75abc6adcea0d50dabc5ecec7d49
aliaa99511 <aliaaattia7@gmail.com>
2021-08-10 13:07:57 +0200

    Updates

---
+++ precache-manifest.f036a8881de9ce5ccc41c7f193977b63.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31b5dd389a138418c024d61d8d4b6d73",
+    "url": "/my-react/index.html"
+  },
+  {
+    "revision": "c6c565883bf7fbb3add8",
+    "url": "/my-react/static/css/2.61431089.chunk.css"
+  },
+  {
+    "revision": "91dd0abdfd1822a917f2",
+    "url": "/my-react/static/css/main.8f47088d.chunk.css"
+  },
+  {
+    "revision": "c6c565883bf7fbb3add8",
+    "url": "/my-react/static/js/2.253e3185.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-react/static/js/2.253e3185.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91dd0abdfd1822a917f2",
+    "url": "/my-react/static/js/main.b40105bf.chunk.js"
+  },
+  {
+    "revision": "e4ee7e697b4b7f1bea58",
+    "url": "/my-react/static/js/runtime-main.e777cef6.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/my-react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/my-react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/my-react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/my-react/static/media/slick.f97e3bbf.svg"
+  }
+]);
